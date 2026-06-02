bde4c82eaa1f6c8b47fd74d56b59af51ff0c43d9
madinecoutymarjorie <madinecoutymarjorie@gmail.com>
2026-06-02 16:45:37 +0200

    Modélisation de données et sécurisation

---
+++ app/Http/Controllers/MorceauController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Morceau;
+use Illuminate\Http\Request;
+
+class MorceauController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Morceau $morceau)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Morceau $morceau)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Morceau $morceau)
+    {
+        //
+    }
+}
